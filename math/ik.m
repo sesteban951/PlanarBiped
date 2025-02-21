02d9723504a3e886208c0e9ba88e82472bcd4ac9
sesteban951 <sesteban@caltech.edu>
2025-02-20 21:45:54 -0800

    IK looks good from base frame

--- math/ik.m
+++ math/ik.m
@@ -11,62 +11,126 @@ params.l0 = l0;
 params.l1 = l1;
 params.l2 = l2;
 
-% random q
-q1_lims = [-pi/2, pi/2];
-q2_lims = [-pi/2, 0];
-q1 = unifrnd(q1_lims(1), q1_lims(2));
-q2 = unifrnd(q2_lims(1), q2_lims(2));
-% q = [q1; q2];
-q = [0.5; -0.7];
-qdot = [0; 0];
+animation = 1;
 
 % syntehsize a joint trajectory for the robot
-t = 0: 0.1 : 3.0;
+t = 0: 0.025 : 7.0;
 q_t = zeros(2, length(t));
+qdot_t = zeros(2, length(t));
+
+f = 0.5;
+w = 2 * pi * f;
+
 for i = 1:length(t)
 
     % sine wave
-    q1 = 0.5 * sin(2 * pi * t(i)) + 0.0;
-    q2 = 0.25 * sin(2 * pi * t(i)) - 0.5;
-    q_t = [q1; q2];
-
-    qdot1 = 2 * pi * 0.5 * cos(2 * pi * t(i));
-    qdot2 = 2 * pi * 0.25 * cos(2 * pi * t(i));
-    qdot_t = [qdot1; qdot2];
+    q1 = 1.0 * sin(w * t(i)) + 0.5;
+    q2 = 1.0 * sin(w * t(i)) - 1.0;
+    qdot1 = 2 * pi * 0.5 * cos(w * t(i));
+    qdot2 = 2 * pi * 0.25 * cos(w * t(i));
     
-
     % populate
-    q_t(:, i) = q_t;
+    q_t(:, i) = [q1; q2];
+    qdot_t(:, i) = [qdot1; qdot2];
 end
 
+
 % compute the forward kinematics
+p_knee_t = zeros(2, length(t));
+p_foot_t = zeros(2, length(t));
+v_foot_t = zeros(2, length(t));
+for i = 1:length(t)
+    [p_knee_t(:, i), p_foot_t(:, i), v_foot_t(:, i)] = fwd_kinmeatics(q_t(:, i), qdot_t(:,i), params);
+end
 
+% compute the inverse kinematics
+q_t_inv = zeros(2, length(t));
+for i = 1:length(t)
+    q_t_inv(:, i) = inv_kinematics(p_foot_t(:, i), params);
+end
 
+% plot the joint angles
+if animation == 0
+    figure('Name', 'Joint Angles');
+    subplot(2, 1, 1);
+    hold on; grid on;
+    plot(t, q_t(1, :), 'r', 'LineWidth', 2);
+    plot(t, q_t_inv(1, :), 'b', 'LineWidth', 2);
+    xlabel('Time (s)');
+    ylabel('Joint Angle (rad)');
+    title('Hip');
+    legend('Forward', 'Inverse');
+
+    subplot(2, 1, 2);
+    hold on; grid on;
+    plot(t, q_t(2, :), 'r', 'LineWidth', 2);
+    plot(t, q_t_inv(2, :), 'b', 'LineWidth', 2);
+    xlabel('Time (s)');
+    ylabel('Joint Angle (rad)');
+    title('Knee');
+    legend('Forward', 'Inverse');
+end
 
 % plot the robot
-figure;
-axis equal; hold on; grid on;
-xline(0);
-yline(0);
-px_max = max([p_foot(1), p_knee(1), 0]);
-px_min = min([p_foot(1), p_knee(1), 0]);
-pz_max = max([p_foot(2), p_knee(2), 0]);
-pz_min = min([p_foot(2), p_knee(2), 0]);
-xlim([px_min - 0.1, px_max + 0.1]);
-ylim([pz_min - 0.1, pz_max + 0.1]);
-xlabel('x (m)');
-ylabel('z (m)');
+if animation == 1
+    figure('Name', 'Robot');
+    axis equal; hold on; grid on;
+    xline(0);
+    yline(0);
+    px_max = max([p_foot_t(1,:), p_knee_t(1,:), 0]);
+    px_min = min([p_foot_t(1,:), p_knee_t(1,:), 0]);
+    pz_max = max([p_foot_t(2,:), p_knee_t(2,:), 0]);
+    pz_min = min([p_foot_t(2,:), p_knee_t(2,:), 0]);
+    xlim([px_min - 0.1, px_max + 0.1]);
+    ylim([pz_min - 0.1, pz_max + 0.1]);
+    xlabel('x (m)');
+    ylabel('z (m)');
 
-% plot the thigh and shin
-plot([0, p_knee(1)], [0, p_knee(2)], 'k', 'LineWidth', 2);
-plot([p_knee(1), p_foot(1)], [p_knee(2), p_foot(2)], 'k', 'LineWidth', 2);
+    % plot the hip
+    plot(0, 0, 'ko', 'MarkerSize', 5, 'MarkerFaceColor', 'r');
 
-% plot the hip, knee and foot
-plot(0, 0, 'ko', 'MarkerSize', 5, 'MarkerFaceColor', 'r');
-plot(p_knee(1), p_knee(2), 'ko', 'MarkerSize', 5, 'MarkerFaceColor', 'r');
-plot(p_foot(1), p_foot(2), 'ko', 'MarkerSize', 5, 'MarkerFaceColor', 'r');
+    idx = 1;
+    tic;
+    while idx < length(t)
 
+        % plot the thigh and shin
+        thigh = plot([0, p_knee_t(1, idx)], [0, p_knee_t(2, idx)], 'k', 'LineWidth', 2);
+        shin = plot([p_knee_t(1, idx), p_foot_t(1, idx)], [p_knee_t(2, idx), p_foot_t(2, idx)], 'k', 'LineWidth', 2);
 
+        % plot the knee and foot
+        knee = plot(p_knee_t(1, idx), p_knee_t(2, idx), 'ko', 'MarkerSize', 5, 'MarkerFaceColor', 'r');
+        foot = plot(p_foot_t(1, idx), p_foot_t(2, idx), 'ko', 'MarkerSize', 5, 'MarkerFaceColor', 'r');
+
+        msg = sprintf('Time: %.3f', t(idx));
+        title(msg);
+
+        drawnow;
+
+        % wait until next iteration
+        while toc < t(idx+1)
+            % do nothing
+        end
+        
+        % increment the index
+        if idx >= length(t)
+            break;
+        else
+            idx = idx + 1;
+            if idx == length(t)
+                % dont delete
+            else
+                delete(thigh);
+                delete(shin);
+                delete(knee);
+                delete(foot);
+            end
+        end
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% AUXILIARY FUNCTIONS
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 % compute the forward kinematics
@@ -92,26 +156,6 @@ function [p_knee, p_foot, v_foot] = fwd_kinmeatics(q, qdot, params)
     v_foot = J * qdot;
 end
 
-% % compute the inverse kinematics
-% function [q, qdot] = inv_kinematics(p_foot, v_foot, params)
-
-%     % link lengths
-%     l1 = params.l1;
-%     l2 = params.l2;
-
-%     % unpack stuff
-%     px = p_foot(1);
-%     pz = p_foot(2);
-
-%     % Inverse kinematics
-%     q1 = atan2(px, -pz);
-%     q2 = acos((px^2 + pz^2 - l1^2 - l2^2) / (2 * l1 * l2));
-
-%     % Jacobian
-%     J = [l1 * cos(q1) + l2 * cos(q1 + q2), l2 * cos(q1 + q2);
-%          l1 * sin(q1) + l2 * sin(q1 + q2), l2 * sin(q1 + q2)];
-%     qdot = pinv(J) * v_foot;
-% end
 
 function q = inv_kinematics(p_foot_des, params)
     % Extract link lengths
@@ -125,12 +169,12 @@ function q = inv_kinematics(p_foot_des, params)
     % Compute q2 using the law of cosines
     c2 = (x^2 + y^2 - l1^2 - l2^2) / (2 * l1 * l2);
     s2 = sqrt(1 - c2^2);  % Choose positive for elbow-up, negative for elbow-down
-    q2 = atan2(s2, c2);
+    q2 = -atan2(s2, c2);
 
     % Compute q1 using the law of sines
     gamma = atan2(y, x);
     beta = atan2(l2 * sin(q2), l1 + l2 * cos(q2));
-    q1 = gamma - beta;
+    q1 = gamma - beta + pi/2;
 
     % Return joint angles
     q = [q1; q2];
