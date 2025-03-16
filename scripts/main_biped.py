9caf3e1f52a8b37090b4295100669a3421a418eb
sesteban951 <sesteban@caltech.edu>
2025-03-16 14:18:21 -0700

    first stab at adding output velocity tracking on whoel biped

--- scripts/main_biped.py
+++ scripts/main_biped.py
@@ -48,12 +48,12 @@ class BipedSimulation:
         # sim parameters
         self.sim_time = 0.0
         self.max_sim_time = config["SIM"]["sim_time"]
-        self.dt_sim = self.model.opt.timestep
         self.hz_render = config["SIM"]["hz_render"]
+        self.dt_sim = self.model.opt.timestep
 
         # model parameters
-        self.l1 = 0.5 # length of the thigh (check that this matches the XML file)
-        self.l2 = 0.5 # length of the shin  (check that this matches the XML file)
+        self.l1 = 0.5      # length of the thigh (check that this matches the XML file)
+        self.l2 = 0.5      # length of the shin  (check that this matches the XML file)
 
         # center of mass state
         self.p_com = 0.0
@@ -64,8 +64,8 @@ class BipedSimulation:
         self.v_rom = 0.0
 
         # desired COM state
-        self.v_joystick = 0.0
-        self.v_des_const = config["HLIP"]["v_des"]
+        self.v_joystick = 0.0                      # (when joystick is available) 
+        self.v_des_const = config["HLIP"]["v_des"] # (when joystick is not available)
 
         # phasing variables
         self.T_SSP = config["HLIP"]["T_SSP"]
@@ -85,11 +85,11 @@ class BipedSimulation:
         self.u = None                                # foot placement w.r.t to stance foot
 
         # desired height
-        self.theta_des = config["HLIP"]["theta_des"]   # desired torso angle
-        self.z_0 = config["HLIP"]["z0"]                      # LIP constant height
+        self.theta_des = config["HLIP"]["theta_des"]         # desired torso angle
+        self.z_0 = config["HLIP"]["z0"]                      # COM constant height
         self.z_apex = config["HLIP"]["z_apex"]               # foot apex height
         self.z_foot_offset = config["HLIP"]["z_foot_offset"] # (foot is round)
-        self.bez_deg = config["HLIP"]["bezier_deg"]               # degree of bezier curve
+        self.bez_deg = config["HLIP"]["bezier_deg"]          # degree of bezier curve
 
         # some hyperbolic trig lambda func
         self.coth = lambda x: (np.exp(2 * x) + 1) / (np.exp(2 * x) - 1)
@@ -109,8 +109,8 @@ class BipedSimulation:
 
         # low level joint gains
         self.kp_H = config["GAINS"]["kp_H"]
-        self.kd_H = config["GAINS"]["kd_H"]
         self.kp_K = config["GAINS"]["kp_K"]
+        self.kd_H = config["GAINS"]["kd_H"]
         self.kd_K = config["GAINS"]["kd_K"]
 
         # camera settings
@@ -167,11 +167,11 @@ class BipedSimulation:
             self.stance_foot = "R"
 
         # compute the forward kinematics
-        _, y_left_W, y_right_W = self.compute_forward_kinematics()
+        _, y_left_W, y_right_W, _, _, _ = self.compute_forward_kinematics()
 
-        # based on the phase variable, assign stance foot position
+        # based on the stance foot variable, assign stance foot position 
         if self.stance_foot == "L":
-            self.p_swing_init = np.array([y_right_W[0], [self.z_foot_offset]]).reshape(2, 1)
+            self.p_swing_init = np.array([y_right_W[0], [self.z_foot_offset]]).reshape(2, 1)  
             self.p_stance = np.array([y_left_W[0], [self.z_foot_offset]]).reshape(2, 1)
 
         elif self.stance_foot == "R":
@@ -200,7 +200,7 @@ class BipedSimulation:
         # Divide by the total mass to get the overall CoM
         com /= total_mass
 
-        # update the COM state
+        # update the COM state (approxaimte velocity using base frame)
         self.p_com = np.array([com[0], com[2]]).reshape(2, 1)
         self.v_com = np.array([self.data.qvel[0], self.data.qvel[1]]).reshape(2, 1)
 
@@ -208,7 +208,7 @@ class BipedSimulation:
     def update_hlip_state(self):
 
         # compute the forward kinematics
-        _, y_left_W, y_right_W = self.compute_forward_kinematics()
+        _, y_left_W, y_right_W, _, _, _ = self.compute_forward_kinematics()
 
         # compute the HLIP position
         if self.stance_foot == "L":
@@ -222,26 +222,24 @@ class BipedSimulation:
     # compute the foot placement based on Raibert
     def compute_foot_placement(self):
 
-        # compute the desired velocity
+        # get the desired velocity
         if self.joystick_available == True:
-            v_des = self.v_joystick
+            self.v_des = self.v_joystick
         else:
-            v_des = self.v_des_const
-
-        print(f"v_des: {v_des:.3f}")
+            self.v_des = self.v_des_const
 
         # compute the desired preimpact HLIP state
-        p_minus_H = (v_des * self.T_tot) / (2 + self.T_DSP * self.sigma_P1)
-        v_minus_H = self.sigma_P1 * (v_des * self.T_tot) / (2 + self.T_DSP * self.sigma_P1)
+        p_minus_H = (self.v_des * self.T_tot) / (2 + self.T_DSP * self.sigma_P1)
+        v_minus_H = self.sigma_P1 * (self.v_des * self.T_tot) / (2 + self.T_DSP * self.sigma_P1)
 
-        # compute the predicted preimpact state of the robot
+        # approximate the preimpact state of the Robot
         x_R = np.array([self.p_rom, self.v_rom]).reshape(2, 1)
         x_minus_R = sp.linalg.expm(self.A * (self.T_SSP - self.T_phase)) @ x_R
         p_minus_R = x_minus_R[0][0]
         v_minus_R = x_minus_R[1][0]
 
         # compute the foot placement relative to the stance foot
-        u_ff = v_des * self.T_SSP
+        u_ff = self.v_des * self.T_SSP
         u_pos = self.Kp_db * (p_minus_R - p_minus_H)
         u_vel = self.Kd_db * (v_minus_R - v_minus_H)
 
@@ -277,7 +275,7 @@ class BipedSimulation:
         # bezier control points
         P = np.array([x_pts, z_pts])
 
-        # want to compute this at normalized time
+        # want to compute this at normalized time, t in [0,1]
         t = self.T_phase / self.T_SSP
 
         # build the bezier curve
@@ -286,65 +284,125 @@ class BipedSimulation:
         
         # evaulate the bezier curve at time t
         p_swing_bez = curve.evaluate(t)
+        pdot_swing_bez = curve.evaluate_hodograph(t)
 
         # track a convex combination of bezier and current foot position
         if self.blend_foot_traj == True:
-            _, y_left_W, y_right_W = self.compute_forward_kinematics()
+
+            # compute the current swing foot position and velocity
+            _, y_left_W, y_right_W, _, ydot_left_W, ydot_right_W = self.compute_forward_kinematics()
+            
+            # set the swing foot position and velocity based on the stance foot
             if self.stance_foot == "L":
                 p_swing_act = y_right_W
+                pdot_swing_act = ydot_right_W
             elif self.stance_foot == "R":
                 p_swing_act = y_left_W
+                pdot_swing_act = ydot_left_W
+
+            # compute the convex combination
             p_swing = (1 - t) * p_swing_act + t * p_swing_bez
+            pdot_swing = (1 - t) * pdot_swing_act + t * pdot_swing_bez
         
         # directly track the bezier curve
         else:
             p_swing = p_swing_bez
+            pdot_swing = pdot_swing_bez
 
-        return p_swing
+        return p_swing, pdot_swing
 
     # update the desired outputs
     def update_output_des(self):
         
         # compute the desired base outputs
-        y_base_W, _, _ = self.compute_forward_kinematics()
-        y_base_W[1] = self.z_0 + self.z_foot_offset
-        y_base_W[2] = self.theta_des
-        y_base_des_W = y_base_W
+        y_base_W, _, _, ydot_base_W, _, _ = self.compute_forward_kinematics()
         
-        # define the desired foot outputs
-        p_swing_W = self.compute_swing_foot_pos()
+        # base position output (leave the x_pos as is since underactuated)
+        y_base_W[1] = self.z_0 + self.z_foot_offset   # base z-position
+        y_base_W[2] = self.theta_des                  # base theta
+        y_base_des_W = y_base_W                  
+        
+        # base velocity output (leave the x_vel as is since underactuated)
+        ydot_base_W[1] = 0.0       # base z-velocity
+        ydot_base_W[2] = 0.0       # base theta velocity
+        ydot_base_W = ydot_base_W
 
-        # make a deep copy of the stance foot
+        # define the desired swing foot position outputs
+        p_swing_W, pdot_swing_W = self.compute_swing_foot_pos()
+
+        # define the desired stance foot velocity outputs
         p_stance_W = self.p_stance
+        pdot_stance_W = np.zeros((2, 1))
 
         # compute the desired foot outputs
         if self.stance_foot == "L":
+            # left foot is stationary
             y_left_des_W = p_stance_W
+            ydot_left_W = pdot_stance_W
+
+            # right foot is moving
             y_right_des_W = p_swing_W
+            ydot_right_W = pdot_swing_W
 
         elif self.stance_foot == "R":
+            # left foot is moving
             y_left_des_W = p_swing_W
-            y_right_des_W = p_stance_W
+            ydot_left_W = pdot_swing_W
 
-        return y_base_des_W, y_left_des_W, y_right_des_W
+            # right foot is stationary
+            y_right_des_W = p_stance_W
+            ydot_right_W = pdot_stance_W
+
+        return y_base_des_W, y_left_des_W, y_right_des_W, ydot_base_W, ydot_left_W, ydot_right_W
+
+    # compute the base to foot jacobian
+    def compute_base_to_foot_jacobian(self, q):
+
+        # unpack the joint angle state
+        q_H = q[0][0]
+        q_K = q[1][0]
+
+        # compute the jacobian
+        J = np.array([[self.l1 * np.cos(q_H) + self.l2 * np.cos(q_H + q_K), self.l2 * np.cos(q_H + q_K)],
+                      [self.l1 * np.sin(q_H) + self.l2 * np.sin(q_H + q_K), self.l2 * np.sin(q_H + q_K)]])
+
+        return J
 
     # compute the forward kinematics in WORLD Frame
     def compute_forward_kinematics(self):
 
-        # unpack the base position
+        # unpack the base position and velocity
         p_base_W = np.array([self.data.qpos[0], self.data.qpos[1]]).reshape(2, 1)
+        pdot_base_W = np.array([self.data.qvel[0], self.data.qvel[1]]).reshape(2, 1)
         theta_W = self.data.qpos[2]
+        thetadot_W = self.data.qvel[2]
+
+        # base rotation matrix and rotation rate
         R = np.array([[np.cos(theta_W), -np.sin(theta_W)],
                       [np.sin(theta_W),  np.cos(theta_W)]])
-        y_base_W = np.array([p_base_W[0], p_base_W[1], [theta_W]]).reshape(3, 1)
+        omega_skew = thetadot_W * np.array([[0, -1],
+                                            [1, 0]])
+        Rdot = omega_skew @ R
 
-        # unpack the joint angles
+        # compute base outputs
+        y_base_W = np.array([p_base_W[0], p_base_W[1], [theta_W]]).reshape(3, 1)
+        ydot_base_W = np.array([pdot_base_W[0], pdot_base_W[1], [thetadot_W]]).reshape(3, 1)
+
+        # unpack the joint angle state
         q_HL = self.data.qpos[3]
         q_KL = self.data.qpos[4]
         q_HR = self.data.qpos[5]
         q_KR = self.data.qpos[6]
+        qdot_HL = self.data.qvel[3]
+        qdot_KL = self.data.qvel[4]
+        qdot_HR = self.data.qvel[5]
+        qdot_KR = self.data.qvel[6]
+        q_L = np.array([q_HL, q_KL]).reshape(2, 1)
+        q_R = np.array([q_HR, q_KR]).reshape(2, 1)
+        qdot_L = np.array([qdot_HL, qdot_KL]).reshape(2, 1)
+        qdot_R = np.array([qdot_HR, qdot_KR]).reshape(2, 1)
 
-        # compute the positions of the feet relative to the base frame
+        # compute the foot outputs
         p_left_B = np.array([self.l1 * np.sin(q_HL) + self.l2 * np.sin(q_HL + q_KL),
                             -self.l1 * np.cos(q_HL) - self.l2 * np.cos(q_HL + q_KL)]).reshape(2, 1)
         p_right_B = np.array([self.l1 * np.sin(q_HR) + self.l2 * np.sin(q_HR + q_KR),
@@ -354,58 +412,88 @@ class BipedSimulation:
         y_left_W = p_left_W
         y_right_W = p_right_W
 
-        return y_base_W, y_left_W, y_right_W
+        # compute the velocities of the feet
+        J_L = self.compute_base_to_foot_jacobian(q_L)
+        J_R = self.compute_base_to_foot_jacobian(q_R)
+        v_left_B = J_L @ qdot_L
+        v_right_B = J_R @ qdot_R
+        ydot_left_W = pdot_base_W + Rdot @ p_left_B + R @ v_left_B     # TODO: check that this is correct
+        ydot_right_W = pdot_base_W + Rdot @ p_right_B + R @ v_right_B  # TODO: check that this is correct
+
+        return y_base_W, y_left_W, y_right_W, ydot_base_W, ydot_left_W, ydot_right_W
 
     # compute inverse kineamtics given feet position in world frame
-    def compute_inverse_kinematics(self, y_base_W, y_left_W, y_right_W):
+    def compute_inverse_kinematics(self, y_base_W,    y_left_W,    y_right_W,
+                                         ydot_base_W, ydot_left_W, ydot_right_W):
 
-        # unpack the base position
+        # unpack the base position and velocity
         p_base_W = np.array([y_base_W[0], y_base_W[1]]).reshape(2, 1)
-        theta_des = y_base_W[2]
+        theta_des = y_base_W[2][0]
+        pdot_base_W = np.array([ydot_base_W[0], ydot_base_W[1]]).reshape(2, 1)
+        thetadot_des = ydot_base_W[2][0]
+
+        # base rotation matrix and rotation rate
         R = np.array([[np.cos(theta_des), -np.sin(theta_des)],
                       [np.sin(theta_des),  np.cos(theta_des)]])
+        omega_skew = thetadot_des * np.array([[0, -1],
+                                              [1, 0]])
+        Rdot = omega_skew @ R
 
         # compute the position of the feet in base frame
-        p_left_W = np.array([y_left_W[0], y_left_W[1]]).reshape(2, 1)
+        p_left_W =  np.array([y_left_W[0],  y_left_W[1]]).reshape(2, 1)
         p_right_W = np.array([y_right_W[0], y_right_W[1]]).reshape(2, 1)
-        p_left_B = (R.transpose() @ (p_left_W - p_base_W)).reshape(2, 1)
+        p_left_B =  (R.transpose() @ (p_left_W  - p_base_W)).reshape(2, 1)
         p_right_B = (R.transpose() @ (p_right_W - p_base_W)).reshape(2, 1)
 
-        # unpack the desired position feet positions in world frame
-        x_L = p_left_B[0][0]
-        z_L = p_left_B[1][0]
-        x_R = p_right_B[0][0]
-        z_R = p_right_B[1][0]
+        # unpack the desired feet positions in world frame
+        px_L = p_left_B[0][0]
+        pz_L = p_left_B[1][0]
+        px_R = p_right_B[0][0]
+        pz_R = p_right_B[1][0]
 
         # knee angle (Left)
-        L = np.sqrt(x_L**2 + z_L**2)
+        L = np.sqrt(px_L**2 + pz_L**2)
         arg = np.clip((L**2 - self.l1**2 - self.l2**2) / (-2 * self.l1 * self.l2),  -1.0, 1.0)
         gamma = np.arccos(arg)
         q_KL = gamma - np.pi
 
         # hip angle (Left)
-        beta = np.arctan2(x_L, -z_L)
+        beta = np.arctan2(px_L, -pz_L)
         arg = np.clip((self.l2**2 - self.l1**2 - L**2) / (-2 * self.l1 * L),  -1.0, 1.0)
         alpha = np.arccos(arg)
         q_HL = beta + alpha
 
         # knee angle (Right)
-        L = np.sqrt(x_R**2 + z_R**2)
+        L = np.sqrt(px_R**2 + pz_R**2)
         arg = np.clip((L**2 - self.l1**2 - self.l2**2) / (-2 * self.l1 * self.l2),  -1.0, 1.0)
         gamma = np.arccos(arg)
         q_KR = gamma - np.pi
         
         # hip angle (Right)
-        beta = np.arctan2(x_R, -z_R)
+        beta = np.arctan2(px_R, -pz_R)
         arg = np.clip((self.l2**2 - self.l1**2 - L**2) / (-2 * self.l1 * L),  -1.0, 1.0)
         alpha = np.arccos(arg)
         q_HR = beta + alpha
 
-        # pack the positions
+        # pack the joint positions
         q_left = np.array([q_HL, q_KL]).reshape(2, 1)
         q_right = np.array([q_HR, q_KR]).reshape(2, 1)
+        q = np.vstack((q_left, q_right))
 
-        return q_left, q_right
+        # compute the velocities of the feet
+        J_L = self.compute_base_to_foot_jacobian(q_left)
+        J_R = self.compute_base_to_foot_jacobian(q_right)
+        pdot_left_W = np.array([ydot_left_W[0], ydot_left_W[1]]).reshape(2, 1)
+        pdot_right_W = np.array([ydot_right_W[0], ydot_right_W[1]]).reshape(2, 1)
+        pdot_left_B =  R.transpose() @ (-pdot_base_W - Rdot @ p_left_B  + pdot_left_W)  # TODO: check that this is correct
+        pdot_right_B = R.transpose() @ (-pdot_base_W - Rdot @ p_right_B + pdot_right_W) # TODO: check that this is correct
+    
+        # pack the joint velocities
+        qdot_left =  np.linalg.pinv(J_L) @ pdot_left_B
+        qdot_right = np.linalg.pinv(J_R) @ pdot_right_B
+        qdot = np.vstack((qdot_left, qdot_right))
+
+        return q, qdot
     
     ############################################### LOW LEVEL ######################################
 
@@ -441,7 +529,7 @@ class BipedSimulation:
         tau_KR = self.kp_H * (q_KR_des - q_KR) + self.kd_K * (qd_KR_des - qd_KR)
 
         # pack the torques
-        tau = [tau_HL, tau_KL, tau_HR, tau_KR]
+        tau = np.array([tau_HL, tau_KL, tau_HR, tau_KR]).reshape(4, 1)
         
         return tau
     
@@ -584,14 +672,13 @@ class BipedSimulation:
             self.update_hlip_state()
 
             # compute desired outputs
-            y_base_des, y_left_des, y_right_des = self.update_output_des()
+            y_base_des, y_left_des, y_right_des, ydot_base_W, ydot_left_W, ydot_right_W = self.update_output_des()
 
             # compute the inverse kinematics
-            q_left_des, q_right_des = self.compute_inverse_kinematics(y_base_des, y_left_des, y_right_des)
+            q_des, qd_des = self.compute_inverse_kinematics(y_base_des,  y_left_des,  y_right_des,
+                                                              ydot_base_W, ydot_left_W, ydot_right_W)
 
             # compute torques
-            q_des = np.array([q_left_des[0], q_left_des[1], q_right_des[0], q_right_des[1]])
-            qd_des = np.array([0.0, 0.0, 0.0, 0.0])
             tau = self.compute_torques(q_des, qd_des)
             self.data.ctrl[0] = tau[0][0]
             self.data.ctrl[1] = tau[1][0]
@@ -599,7 +686,7 @@ class BipedSimulation:
             self.data.ctrl[3] = tau[3][0]
 
             # compute the forward kinematics
-            y_base_act, y_left_act, y_right_act = self.compute_forward_kinematics()
+            y_base_act, y_left_act, y_right_act, _, _, _ = self.compute_forward_kinematics()
 
             # update the camera to track the COM
             self.update_camera_to_com(cam)
@@ -683,7 +770,8 @@ class BipedSimulation:
 if __name__ == "__main__":
     
     # load the config
-    with open("../config/biped.yaml", "r") as stream:
+    config_path = "../config/biped.yaml"
+    with open(config_path, "r") as stream:
         config = yaml.safe_load(stream)
 
     # create the biped simulation
