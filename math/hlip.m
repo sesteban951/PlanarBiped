f36ddaf2c483404c89398c7a2c8b6003285c5e5f
sesteban951 <sesteban@caltech.edu>
2025-03-23 14:47:37 -0700

    code to see if the Robot matches HLIP dynamics

--- math/hlip.m
+++ math/hlip.m
@@ -26,7 +26,7 @@ B_S2S = exp_A_SSP_T_SSP * [-1;
 % LQR controller
 Q_x = diag([1, 0.1]);
 Q_u = 1;
-K_lqr = -lqrd(A_S2S, B_S2S, Q_x, Q_u, 1);
+K_lqr = -lqrd(A_S2S, B_S2S, Q_x, Q_u, T_SSP);
 
 % deadbeat controller
 K_db = [1, T_DSP + (1/lam) * coth(T_SSP * lam)];
@@ -44,15 +44,14 @@ Q = eye(2);
 A_cl = (A_S2S + B_S2S * K);
 P = dlyap(A_cl, Q);
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % initial condition
-x0 = [0.25;
-      0.75];
+x0 = [0.2;
+      -0.2];
 
 % simulate forward in time
-n_steps = 5;
+n_steps = 6;
 x_t = zeros(2, n_steps);
 u_t = zeros(1, n_steps-1);
 V_t = zeros(1, n_steps);
@@ -74,12 +73,57 @@ end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% compute the HLIP gradient fields
+max_val = max(abs(x_t(:)));
+x_range = linspace(-max_val - 0.1, max_val + 0.1, 25);
+y_range = linspace(-max_val - 0.1, max_val + 0.1, 25);
+[X1, X2] = meshgrid(x_range, y_range);
+
+% compute the vector fields at each point
+vecotr_field = zeros(size(X1, 1), size(X1, 2), 2);
+for i = 1:size(X1, 1)
+    for j = 1:size(X1, 2)
+        
+        % get the state
+        x = [X1(i, j);
+             X2(i, j)];
+        
+        % compute the vecotr field
+        vf = A_SSP * x;
+
+        % normalize the vector field
+        norm_vf = norm(vf);
+
+        % store the vector field
+        vecotr_field(i, j, :) = vf / norm_vf;
+    end
+end
+
+% compute the continuous phase dynamics
+dt = 0.01;
+t_continuous = 0:dt:T_SSP;
+x_t_continuous = zeros(length(t_continuous), 2, n_steps);
+for i = 1:n_steps
+    
+    % initial condition 
+    x0 = x_t(:, i);
+
+    % compute the continuous phase dynamics
+    for j = 1:length(t_continuous)
+        e_ASSP = expm(A_SSP * t_continuous(j));
+        x_t_continuous(j, :, i) = (e_ASSP * x0);
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % plot the results
 figure(1);
 
 xlims = [min(x_t(1, :)) - 0.1, max(x_t(1, :)) + 0.1];
 ylims = [min(x_t(2, :)) - 0.1, max(x_t(2, :)) + 0.1];
 
+% plot the discrete dynamics
 subplot(2, 2, 1);
 grid on; hold on;
 xlim(xlims);
@@ -92,6 +136,7 @@ xlabel('p (m)');
 ylabel('v (m/s)');
 title('Dsicrete Dynamics');
 
+% plot the discrete inputs
 subplot(2, 2, 3);
 grid on; hold on;
 yline(0);
@@ -101,7 +146,20 @@ ylabel('u (m)');
 xticks(1:n_steps-1);
 title('Control Input');
 
-subplot(2 ,2, [2, 4]);
+% plot the continuous flows
+subplot(2, 2, 2);
+grid on; hold on;
+xlim([x_range(1), x_range(end)]);
+ylim([y_range(1), y_range(end)]);
+xline(0); yline(0);
+quiver(X1, X2, vecotr_field(:, :, 1), vecotr_field(:, :, 2), 'r', 'LineWidth', 0.5);
+for i = 1:n_steps
+    plot(x_t_continuous(:, 1, i), x_t_continuous(:, 2, i), 'k-','LineWidth', 2); % trajectory
+end
+plot(x_t(1, :), x_t(2, :), 'ko','LineWidth', 2); % trajectory
+
+% plot the lyapunov function
+subplot(2 ,2, 4);
 grid on; hold on; axis equal;
 x1_max = max(abs(x_t(1, :)));
 x2_max = max(abs(x_t(2, :)));
@@ -125,3 +183,4 @@ zlabel('V(x)');
 title('Quadratic Function V = x^T P x');
 view(3);
 grid on;
+
--- plot/plot_rom.m
+++ plot/plot_rom.m
@@ -22,26 +22,86 @@ config = yaml.loadFile(config_file_path);
 
 % some parameters
 g = 9.81;
-T_SSP = config.HLIP.T_SSP;
 z0_des = config.HLIP.z0;
+v_des = config.HLIP.v_des;
+
+T_SSP = config.HLIP.T_SSP;
+T_DSP = 0.0;
+T_tot = T_SSP + T_DSP;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ROBOT Emperical
 
 % time window of interest
-t_interval = [t(1), t(end)];
-% t_interval = [1, 3];
+% t_interval = [t(1), t(end)];
+t_interval = [t(end) - 2, t(end)];
 idx = find(t >= t_interval(1) & t <= t_interval(2));
 
 t = t(idx);
-
 p = p(idx);
 v = v(idx);
 u = u(idx);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% HLIP Theoretical
+
+% continuous dybnamics of the HLIP
+lam = sqrt(g/z0_des);
+Kp_db = 1;
+Kd_db = (1/lam) * coth(lam * T_SSP);
+K = [Kp_db, Kd_db];
+A_SSP = [0,     1;
+         lam^2, 0];
+
+% discrete S2S dynamics
+exp_A_SSP_T_SSP = expm(A_SSP * T_SSP);
+A_S2S = exp_A_SSP_T_SSP * [1, T_DSP;
+                           0, 1];
+B_S2S = exp_A_SSP_T_SSP * [-1;
+                            0];
+A_S2S_cl = (A_S2S + B_S2S * K);
+
+% compute the desired HLIP preimpact
+sigma_P1 = lam * coth(0.5 * lam * T_SSP);
+p_minus_H = (v_des * T_tot) / (2 + T_DSP * sigma_P1);
+v_minus_H = sigma_P1 * (v_des * T_tot) / (2 + T_DSP * sigma_P1);
+
+% compute some phase plots
+x_max = max([p; p_minus_H]);
+x_min = min([p; p_minus_H]);
+y_max = max([v; v_minus_H]);
+y_min = min([v; v_minus_H]);
+x_range = linspace(x_min, x_max, 25);
+y_range = linspace(y_min, y_max, 25);
+[X1, X2] = meshgrid(x_range, y_range);
+
+% compute the vector fields at each point
+vecotr_field = zeros(size(X1, 1), size(X1, 2), 2);
+for i = 1:size(X1, 1)
+    for j = 1:size(X1, 2)
+        
+        % get the state
+        x = [X1(i, j);
+             X2(i, j)];
+        
+        % compute the vecotr field
+        vf = A_SSP * x;
+
+        % normalize the vector field
+        norm_vf = norm(vf);
+
+        % store the vector field
+        vecotr_field(i, j, :) = vf / norm_vf;
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% plot all the individual ROM flows
+figure(1);
 
 % plot the results
-subplot(2, 3, 1);
+subplot(3, 1, 1);
 grid on; hold on;
 yline(0);
 plot(t, p, 'LineWidth', 2);
@@ -49,7 +109,7 @@ xlabel('t (s)');
 ylabel('p (m)');
 title('p = p_c - p_s');
 
-subplot(2, 3, 2);
+subplot(3, 1, 2);
 grid on; hold on;
 yline(0);
 plot(t, v, 'LineWidth', 2);
@@ -57,7 +117,7 @@ xlabel('t (s)');
 ylabel('v (m/s)');
 title('v');
 
-subplot(2, 3, 3);
+subplot(3, 1, 3);
 grid on; hold on;
 yline(0);
 plot(t, u, 'LineWidth', 2);
@@ -65,10 +125,21 @@ xlabel('t (s)');
 ylabel('u (m)');
 title('u');
 
-subplot(2, 3, [4, 6]);
-grid on; hold on;
-xline(0); yline(0);
-plot(p, v, 'k.', 'LineWidth', 2);
+% plot the continuous phase plot
+figure(2);
+grid on; hold on; axis equal;
 xlabel('p (m)');
 ylabel('v (m/s)');
-title('p vs. v');
+
+% plot the vector fields
+streamslice(X1, X2, vecotr_field(:, :, 1), vecotr_field(:, :, 2));
+
+% end and start points
+plot(p(1), v(1), 'go', 'MarkerSize', 10, 'LineWidth', 2); % start
+plot(p(end), v(end), 'ro','MarkerSize', 10, 'LineWidth', 2); % end
+
+% HLIP target point
+plot(p_minus_H, v_minus_H, 'pentagram', 'MarkerSize', 10, 'LineWidth', 2); % target
+
+% plot the actual trajectory
+plot(p, v, 'k', 'LineWidth', 2);
